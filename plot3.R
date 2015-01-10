76018596d56e62f4b3af0a83d91f0a6051130881
rekhaumesh <phdstudentumesh@yahoo.co.in>
2015-01-10 14:19:30 +0530

    PLOT3.r

---
+++ plot3.R
@@ -0,0 +1,25 @@
+
+#Read the Datafile (common code for all the plots)
+data<-read.table("household_power_consumption.txt",sep=";",stringsAsFactors=FALSE,header=TRUE)
+#COPY data to data2
+data2<-data
+data2$Date<-as.Date(data2$Date,"%d/%m/%Y")
+myData<-data2[data2$Date >= "2007-2-1" & data2$Date <= "2007-2-2", ]
+mysubData<-subset(data2, Date >= "2007-2-1" & Date <= "2007-2-2")
+
+
+#PLOT 3
+myData$Sub_metering_1<-as.numeric(myData$Sub_metering_1)
+myData$Sub_metering_2<-as.numeric(myData$Sub_metering_2)
+png("plot3.png",bg="transparent")
+plot(myData$DateTime, myData$Sub_metering_1, col="red",
+     ylab="Energy Sub metering",type="l", xlab="")
+lines(myData$DateTime, myData$Sub_metering_2,col="green")
+lines(myData$DateTime, myData$Sub_metering_3,col="blue")
+legend("topright",c("sub_metering_1",
+                    + "sub_metering_2","sub_metering_3"),col=c("red","green","blue"),lwd=1)
+dev.off()
+
+
+
+
