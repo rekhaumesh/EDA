47282f2d9764330ba17612c139a6d45b6acc67c8
rekhaumesh <phdstudentumesh@yahoo.co.in>
2015-01-10 20:07:27 +0530

    plot2.R
    updated file

--- plot2.R
+++ plot2.R
@@ -3,9 +3,21 @@
 data<-read.table("household_power_consumption.txt",sep=";",stringsAsFactors=FALSE,header=TRUE)
 #COPY data to data2
 data2<-data
+#data2$Date<-as.Date(data2$Date,"%d/%m/%Y")
+#myData<-data2[data2$Date >= "2007-2-1" & data2$Date <= "2007-2-2", ]
+#mysubData<-subset(data2, Date >= "2007-2-1" & Date <= "2007-2-2")
+#Preparing variables for the plot 
 data2$Date<-as.Date(data2$Date,"%d/%m/%Y")
 myData<-data2[data2$Date >= "2007-2-1" & data2$Date <= "2007-2-2", ]
 mysubData<-subset(data2, Date >= "2007-2-1" & Date <= "2007-2-2")
+
+myData$DateTime<-paste(myData$Date,myData$Time)
+myData$DateTime<-strptime(myData$DateTime, format = "%Y-%m-%d %H:%M:%S")
+myData$Global_active_power<-as.numeric(myData$Global_active_power)
+myData$Sub_metering_1<-as.numeric(myData$Sub_metering_1)
+myData$Sub_metering_2<-as.numeric(myData$Sub_metering_2)
+myData$Voltage<-as.numeric(myData$Voltage)
+myData$Global_reactive_power<-as.numeric(myData$Global_reactive_power)
 
 
 #PLOT2
